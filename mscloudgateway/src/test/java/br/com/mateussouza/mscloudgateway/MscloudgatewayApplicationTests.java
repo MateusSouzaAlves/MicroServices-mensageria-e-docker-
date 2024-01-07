b9a62f9bfbb9d6b1bce1771e75babee503c2e004
MateusSouzaAlves <javaamoavida@gmail.com>
2024-01-07 15:37:08 -0300

    Criando o Cloud Gateway

---
+++ mscloudgateway/src/test/java/br/com/mateussouza/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateussouza.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
